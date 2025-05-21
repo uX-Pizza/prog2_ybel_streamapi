69f7f490016da80b41787c417b9bb996e835242e
Thore Schwier <me@thore-schwier.com>
2025-05-21 18:35:38 +0200

    Change Main#ifmCps to use stream

--- src/main/java/streamapi/Main.java
+++ src/main/java/streamapi/Main.java
@@ -2,6 +2,7 @@ package streamapi;
 
 import java.io.InputStream;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /** Starter for the stream api task. */
 public class Main {
@@ -52,17 +53,7 @@ public class Main {
      * @return Set of credit points of all IFM students
      */
     public static Set<Integer> ifmCps(List<Student> studentList) {
-        // TODO
-        Set<Integer> result = new HashSet<>();
-        Integer i = 0;
-        for (Student v : studentList) {
-            if (v.isIFM()) {
-                i = v.cps();
-                result.add(i);
-            }
-        }
-
-        return result;
+        return studentList.stream().filter(Student::isIFM).map(Student::cps).collect(Collectors.toCollection(HashSet::new));
     }
 
     /**
