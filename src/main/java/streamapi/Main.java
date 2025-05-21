d8682c95ed5fa374086ed75c14482db14a0a55ac
Thore Schwier <me@thore-schwier.com>
2025-05-21 18:24:11 +0200

    Change Main#student to use streams

--- src/main/java/streamapi/Main.java
+++ src/main/java/streamapi/Main.java
@@ -39,13 +39,7 @@ public class Main {
      * @return Sum of credit points of all students
      */
     public static Integer students(List<Student> studentList) {
-        // TODO
-        Integer sum = 0;
-        for (Student s : studentList) {
-            sum += s.cps();
-        }
-
-        return sum;
+        return studentList.stream().map(Student::cps).reduce(0, Integer::sum);
     }
 
     /**
