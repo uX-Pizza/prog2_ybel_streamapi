350f0f9d1a458141c0ab5f1dc73e01559d1ec0b6
Thore Schwier <me@thore-schwier.com>
2025-05-21 19:08:40 +0200

    Change Main#random to use streams

--- src/main/java/streamapi/Main.java
+++ src/main/java/streamapi/Main.java
@@ -58,21 +58,7 @@ public class Main {
      */
     public static List<Integer> random() {
         Random r = new Random();
-
-        // TODO
-        List<Integer> randomIntegers = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            randomIntegers.add(r.nextInt(10));
-        }
-
-        List<Integer> returnList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            if (randomIntegers.get(i) % 2 == 0) {
-                returnList.add(randomIntegers.get(i) * randomIntegers.get(i));
-            }
-        }
-
-        return returnList;
+        return r.ints(10, 0, 10).boxed().filter(i -> i%2 == 0).map(i -> i*i).toList();
     }
 
     /**
